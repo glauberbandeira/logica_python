4af2b061da72d85335e84a9b1fc73d9627ae573a
Glauber Bandeira <glauberbandeira_@hotmail.com>
2023-01-15 01:29:14 +1100

    commit ex05

---
+++ ex05.py
@@ -0,0 +1,7 @@
+horas_trabalhadas = int(input('Insira a quantidade de horas você que trabalhou: '))
+
+valor_hora = 30
+
+calcular_salario = horas_trabalhadas * valor_hora
+
+print(f'O seu salário esse mês é de: R${calcular_salario}')
