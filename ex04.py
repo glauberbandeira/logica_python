b9c48ca2c315e3c0287210b608145fde534a05ec
Glauber Bandeira <glauberbandeira_@hotmail.com>
2023-01-15 01:23:20 +1100

    commit ex04

---
+++ ex04.py
@@ -0,0 +1,5 @@
+metros = float(input("Valor em metro: "))
+
+converter_centimetros = round(metros * 100)
+
+print(f'O valor de {metros} metros, convertido em centimetros, fica {converter_centimetros} cm.')
