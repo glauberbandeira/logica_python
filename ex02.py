678b018ab7563cd15aae91b35013115d10df2e8a
Glauber Bandeira <glauberbandeira_@hotmail.com>
2023-01-15 00:28:21 +1100

    commit ex02

---
+++ ex02.py
@@ -0,0 +1,6 @@
+estoque_minimo = int(input('Quantidade minima de estoque: '))
+estoque_maximo = int(input('Quantidade maxima de estoque: '))
+
+estoque_medio = (estoque_minimo + estoque_maximo) / 210
+
+print(f'O estoque médio é de {estoque_medio}')
