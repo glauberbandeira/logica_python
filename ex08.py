15862ab85ef2a7b4748002c66e9c281d9143825f
Glauber Bandeira <glauberbandeira_@hotmail.com>
2023-01-16 00:55:52 +1100

    commit ex 08 and 09

---
+++ ex08.py
@@ -0,0 +1,7 @@
+n = int(input("Digite um numero: "))
+
+if n > 100:
+    print(n)
+else:
+    n = 0
+    print(f'Valor abaixo de 100, se torna {n}')
---
+++ ex09.py
@@ -0,0 +1,8 @@
+n = int(input("Digite um número: "))
+
+if n > 0:
+    a = n
+    print(f'Valor positivo: {a}')
+else:
+    b = n
+    print(f'Valor negativo: {b}') 
