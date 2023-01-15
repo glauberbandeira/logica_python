fedff8424a4da6b855e37b8059e06ab604d6dc2b
Glauber Bandeira <glauberbandeira_@hotmail.com>
2023-01-16 00:49:09 +1100

    commit ex07

---
+++ ex07.py
@@ -0,0 +1,6 @@
+altura = float(input("Qual a sua altura? "))
+
+calculo_peso_ideal = (72.7 * altura) - 58
+peso_ideal = calculo_peso_ideal
+
+print(f"De acordo com sua altura seu peso ideal é de: {peso_ideal}")
