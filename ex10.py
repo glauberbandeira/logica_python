365907ebc1d60d2970673c6caffa596a949845fb
Glauber Bandeira <glauberbandeira_@hotmail.com>
2023-01-16 21:58:32 +1100

    commit ex10

---
+++ ex10.py
@@ -0,0 +1,11 @@
+p = 0
+i = 0
+
+num = int(input("Digite um número: "))
+
+if (num % 2 == 0):
+    p = num
+    print(f'O numero {p} é par.')
+else:
+    i = num
+    print(f'O numero {i} é impar.')
