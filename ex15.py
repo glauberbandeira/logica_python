33f6349d2d95833f60869b520028310b0ae9869b
Glauber Bandeira <glauberbandeira_@hotmail.com>
2023-01-20 23:38:06 +1100

    commit ex15

---
+++ ex15.py
@@ -0,0 +1,12 @@
+num = int(input("Digite um número: "))
+
+if (num % 2 == 0):
+    p = num
+    print(f'O número {p} é par.')
+else:
+    i = num
+    print(f'O número {i} é impar.')
+if (num > 0):
+    print('Número é maior que zero')
+else:
+     print('Número é menor que zero')
