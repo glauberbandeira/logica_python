1c4de4f7ba883c1d33264211ebabc7eac2ce7b47
Glauber Bandeira <glauberbandeira_@hotmail.com>
2023-01-21 15:33:36 +1100

    commit ex19 explication

--- ex19.py
+++ ex19.py
@@ -1,3 +1,5 @@
+# Faça um algoritimo que conte de 1 a 100 e a cada múltiplo de 10 emita uma mensagem: Múltiplo de 10.
+# # Make an algorithm that counts from 1 to 100 and every multiple of 10 sends a message: Multiple of 10.
 for n in range(1, 101):
     print(n)
     if n % 10 == 0:
