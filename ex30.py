ee8654b3a00475a9c9e5d882fa6a89d060bacc5e
Glauber Bandeira <glauberbandeira_@hotmail.com>
2023-11-08 22:03:32 +1100

    commit ex30

---
+++ ex30.py
@@ -0,0 +1,6 @@
+numero = int(input('Me diga um número qualquer: '))
+resultado = numero % 2
+if resultado == 0:
+    print(f'O numero {numero} é PAR')
+else:
+    print(f'O numero {numero} é ÍMPAR')
